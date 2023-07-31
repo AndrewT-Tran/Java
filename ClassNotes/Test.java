e7d96a17a131d854433585f0f8951b875d59f350
Andrew T Tran <me@andrewtran-shi.dev>
2023-07-31 14:42:53 -0700

    day one done

--- ClassNotes/Test.java
+++ ClassNotes/Test.java
@@ -1,5 +1,7 @@
+import java.util.ArrayList;
+
 public class Test {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         VoiceMessageUtil vmu = new VoiceMessageUtil();
         // we have to call the class and then the method
         // we are basically importing this class into this file
@@ -7,5 +9,35 @@ public class Test {
         System.out.println(vmu.greeting("Andrew"));
         System.out.println(vmu.greeting2("Andrew", 26));
         vmu.getCurrentData();
+
+        int[] messagesPerDay = new int[4];
+        messagesPerDay[0] = 10;
+        messagesPerDay[1] = 20;
+        messagesPerDay[2] = 30;
+        messagesPerDay[3] = 40;
+        // int[] messagesPerDay1 = {5, 5 ,5};
+        // messagesPerDay1[0]=8;
+        String[] messages = { "String1", "String2", "String3" };
+        System.out.println(vmu.totalMessages(messagesPerDay));
+        // to import the method totalmessages we reference it by the class name vmu is
+        // declared above
+        vmu.printMessages(messages);
+        ArrayList<String> todoList = new ArrayList<String>();
+        todoList.add("Clean the house");
+        todoList.add("Wash the dishes");
+        vmu.printMessages(todoList);
+
+        int days = 7;
+        double costOfLiving = 1000.00;
+        // no loss of data
+        double totalCostOfLiving = days * costOfLiving;
+        System.out.println(totalCostOfLiving);
+        // int => double
+
+        // postive loss of data
+        int totalCostOfLivingForWeek = (days * (int) costOfLiving);
+        // (days * costOfLiving); will give an error becuase there might be a data loss
+        // double => int
+        System.out.println(totalCostOfLivingForWeek);
     }
 }
--- ClassNotes/VoiceMessageUtil.java
+++ ClassNotes/VoiceMessageUtil.java
@@ -1,21 +1,58 @@
 import java.util.Date;
+import java.util.ArrayList;
+
 public class VoiceMessageUtil {
-    //classes are made up of attributes and methods
-    public String greeting(){
-        //public means we can call this method outside of the class and we are returning a string
+    // classes are made up of attributes and methods
+    public String greeting() {
+        // public means we can call this method outside of the class and we are
+        // returning a string
         return "Hello You";
     }
-    public String greeting (String name ){
+
+    public String greeting(String name) {
         return "Hello " + name + "!";
 
-    }     
-    public String greeting2 (String name, int age){
-        return "Hello " + name + " you are, " + age;}
-    public String greeting3 (String name, int age){
+    }
+
+    public String greeting2(String name, int age) {
+        return "Hello " + name + " you are, " + age;
+    }
+
+    public String greeting3(String name, int age) {
         return String.format("Hello %s you are %d years old", name, age);
     }
-    public void getCurrentData(){
+
+    public void getCurrentData() {
         Date today = new Date();
         System.out.println(today);
-    }     
+    }
+
+    public int totalMessages(int[] intArr) {
+        int total = 0;
+        for (int i = 0; i < intArr.length; i++) {
+            total += intArr[i];
+        }
+        return total;
+    }
+
+    public void printMessages(String[] messages) {
+        for (int i = 0; i < messages.length; i++) {
+            System.out.println(messages[i]);
+        }
+        // enhanced for loop
+        for (String message : messages) {
+            // String <variable name> : <array name>
+
+            System.out.println(message);
+        }
+    }
+
+    public void printMessages(ArrayList<String> messages) {
+        for (int i = 0; i < messages.size(); i++) {
+            System.out.println(messages.get(i));
+        }
+        for (String message : messages) {
+            System.out.println(message);
+        }
+    }
 }
