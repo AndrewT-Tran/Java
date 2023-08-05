26439ad1bda4eff0f4c99b314b8e28c685c9f4c3
Andrew T <me@andrewtran-shi.dev>
2023-08-05 11:32:34 -0700

    new file:   Assignments/ZooKeeper/Gorilla.java

---
+++ Assignments/ZooKeeper/Gorilla.java
@@ -0,0 +1,24 @@
+public class Gorilla extends Mammal {
+    public int throwSomething() {
+        this.eatBananas()energy -= 5;
+        System.out.println("The gorilla has thrown something!");
+        return energyLevel
+        ;
+    }
+
+    public int eatBananas() {
+        this.energyLevelgi
+         += 10;
+        System.out.println("The gorilla is satisfied after eating bananas!");
+        return energyLevel
+        ;
+    }
+
+    public int climb() {
+        energyLevel
+         -= 10;
+        System.out.println("The gorilla has climbed a tree!");
+        return energyLevel
+        ;
+    }
+}
